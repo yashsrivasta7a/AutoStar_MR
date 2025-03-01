2ec3ba7b93bdb5bfa882f67ccfdf4f16ca6369cd
Yash Srivastava <ysyashmks@gmail.com>
2025-03-01 11:49:18 +0530

    AutoStar

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing AutoStar MR...
+git clone https://github.com/yashsrivasta7a/AutoStar_MR.git
+cd AutoStar_MR
+echo Please enable Developer Mode in Chrome and load the extension manually from:
+echo %cd%
+pause
