2bfea37519b8775f3b10aa756393d021731fb8bb
Yash Srivastava <ysyashmks@gmail.com>
2025-04-23 21:08:47 +0530

    Add new logo images: logo/16.jpg and logo/48.jpg

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function selectAllFiveStars() {
-            document.querySelectorAll("input.star-5").forEach(star => {
-                star.checked = true;
-                star.dispatchEvent(new Event("input", { bubbles: true }));
-            });
-        }
-        
-        setTimeout(selectAllFiveStars, 500);
-        
+    document.querySelectorAll("input.star-5").forEach(star => {
+        star.checked = true;
+        star.dispatchEvent(new Event("input", { bubbles: true }));
+    });
+}
+
+document.getElementById("select-stars-btn").addEventListener("click", selectAllFiveStars);
